ee424b45fa98ba4fe1a4e0ed8c3ee0d0d34a1ee9
chaitanyashimpi <chaitanyashimpi2001@gmail.com>
2024-01-01 17:59:28 +0530

    Completed the work of header and footer

--- src/components/NotificationBell/BellNotification.jsx
+++ src/components/NotificationBell/BellNotification.jsx
@@ -1,12 +1,19 @@
+import { useState } from 'react';
 import style from './bell.module.css'
 
 const BellNotification = () => {
+
+  const [notification, setNotification] = useState(true);
+
   return (
-    <div className={style.bell}>
-        <i className="far fa-bell"></i>
+    <div className={style.bell} onClick={() => setNotification(false)}>
+      <i className="far fa-bell">
+      {notification ? <span className={style.beller}></span> : ""}
+      </i>
+        
+        
     </div>
   )
-  
 }
 
 export default BellNotification
