a44319703615dd516cbac348ae06b9c19c5fa9b1
Chaitanya Shimpi <chaitanyashimpi2001@gmail.com>
2024-03-10 15:12:43 +0000

    Changed the design of contact hero section

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -14,12 +14,12 @@ const Contact = () => {
         <div className={style.leftContactHeader}>
           <HeadingTitle title="Start a Project"/>
           <h2 className={style.contactme}>
-            <span>Contact Us</span> & let's get started by growing your business online with us...
+            <span>Contact</span> & grow your business online with us...
           </h2>
-          {/* <div className={style.contactHeadButtons}>
-            <HollowButton text="text"/>
-            <BoldButton text="Free 15-min Consult"/>
-          </div> */}
+          
+          {/* Form Section */}
+
+
         </div>
         {/* <marquee className={style.moveTest}> */}
         <div className={style.movingTestimonial}>
@@ -39,7 +39,9 @@ const Contact = () => {
             <Testimonial review="Lorem ipsum dolor sit amet consectetur adipisicing elit. Et nam dolorum tenetur cupiditate, quae provident adipisci fuga ad? Ratione quas expedita aliquid eos, eius ut quos fugiat sit id cupiditate!" img="../../assets/image/CTA.png" name="Chaitanya Shimpi" title="CEO, Cratonik"/>
             <Testimonial review="Lorem ipsum dolor sit amet consectetur adipisicing elit. Et nam dolorum tenetur cupiditate, quae provident adipisci fuga ad? Ratione quas expedita aliquid eos, eius ut quos fugiat sit id cupiditate!" img="../../assets/image/CTA.png" name="Chaitanya Shimpi" title="CEO, Cratonik"/>
           </Marquee>
-
+          <div className={style.testinomialGradient}>
+          </div>
+            <div className={style.testinomialGradient2}></div>
         </div>
         {/* </marquee> */}
 
